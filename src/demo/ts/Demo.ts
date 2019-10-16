f90f7d5c9e68bdee0bf33197fab8ffa71f484f8b
fernando <Insoutt>
2019-10-16 00:38:13 -0500

    Add mathquill_editor_group, mathquill_editor_button_bar and mathquill_editor_button_groups keys

--- src/demo/ts/Demo.ts
+++ src/demo/ts/Demo.ts
@@ -7,5 +7,60 @@ Plugin();
 tinymce.init({
     selector: "textarea.tinymce",
     plugins: "code mathquill-editor",
-    toolbar: "mathquill-editor"
+    toolbar: "mathquill-editor",
+    mathquill_editor_group: "basic",
+    mathquill_editor_button_bar: "\\times y^2",
+    mathquill_editor_button_groups: {
+        basic: [
+            {
+                name: "Numbers",
+                buttons: "1 2 3 4 5 6 7 8 9 0"
+            },
+            {
+                name: "Arithmetic and Units",
+                buttons:
+                    "+ - \\times \\div \\pm \\overline \\cdot / $ \\degree %"
+            }
+        ],
+        intermediate: [
+            {
+                name: "Exponents, Roots, Logs",
+                buttons: [
+                    {
+                        text: "y^x",
+                        latex: "^"
+                    },
+                    {
+                        text: "\\sqrt{x}",
+                        latex: "\\sqrt"
+                    },
+                    {
+                        text: "\\nthroot[3]{x}",
+                        latex: "\\thirdroot"
+                    },
+                    {
+                        text: "\\nthroot[n]{x}",
+                        latex: "\\nthroot"
+                    },
+                    {
+                        text: "e^x"
+                    },
+                    {
+                        text: "\\ln"
+                    },
+                    {
+                        text: "\\log"
+                    },
+                    {
+                        text: "\\log_b"
+                    }
+                ]
+            },
+            {
+                name: "Relations",
+                buttons:
+                    "= \\neq \\sim \\not\\sim \\lt \\gt \\approx \\not\\approx \\le \\ge \\simeq \\not\\simeq \\therefore"
+            }
+        ]
+    }
 });
