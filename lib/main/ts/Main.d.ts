a85e65b175c679aba134769db4996c6fa59cc6c7
fernando <Insoutt>
2019-12-15 15:50:07 -0500

    Build plugin

--- lib/main/ts/Main.d.ts
+++ lib/main/ts/Main.d.ts
@@ -2,5 +2,5 @@ export {};
 /*******
  * DO NOT EXPORT ANYTHING
  *
- * IF YOU DO ROLLUP WILL LEAVE A `mathquill-editor` GLOBAL ON THE PAGE
+ * IF YOU DO ROLLUP WILL LEAVE A `equation-editor` GLOBAL ON THE PAGE
  *******/
